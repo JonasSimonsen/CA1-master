54ef43d5a76e113d7b4b35379a756814b61e4dde
YoloLigeLidtKodeDer <clind_14@hotmail.com>
2015-09-09 09:38:48 +0200

    gui fun

--- src/gui/ConnectScreen.java
+++ src/gui/ConnectScreen.java
@@ -7,13 +7,13 @@ import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class GUI extends javax.swing.JFrame implements Observer {
+public class ConnectScreen extends javax.swing.JFrame implements Observer {
 
     private int port = 9090;
     private String ip = "localhost";
     ChatClient CC;
 
-    public GUI() {
+    public ConnectScreen() {
         initComponents();
         CC = new ChatClient();
         CC.addObserver(this);
@@ -80,7 +80,7 @@ public class GUI extends javax.swing.JFrame implements Observer {
         try {
             CC.connect(ip, port);
         } catch (IOException ex) {
-            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ConnectScreen.class.getName()).log(Level.SEVERE, null, ex);
         }      
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -101,19 +101,22 @@ public class GUI extends javax.swing.JFrame implements Observer {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConnectScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConnectScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConnectScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConnectScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUI().setVisible(true);
+                new ConnectScreen().setVisible(true);
             }
         });
     }
